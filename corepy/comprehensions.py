040dad528ad08a3ac41babb072761bf7ee9426c2
Petar Marinov <mpetar@vmware.com>
2022-08-15 12:36:53 +0300

    comprehensions

--- corepy/comprehensions.py
+++ corepy/comprehensions.py
@@ -1,5 +1,9 @@
 # [expr(item) for item in iterable]
+def generate_item(item):
+    return len(item)
+
+
 words = "Why sometimes I have believed as many as six items before breakfast".split()
-co = [len(item) for item in words]
+co = [generate_item(item) for item in words]
 
 print(co)  # [3, 9, 1, 4, 8, 2, 4, 2, 3, 5, 6, 9]
