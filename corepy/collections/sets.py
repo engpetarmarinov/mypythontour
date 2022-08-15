fd91f222becba0ccde438507733eb379a559a2b2
Petar Marinov <mpetar@vmware.com>
2022-08-15 10:43:43 +0300

    Sets algebra

--- corepy/collections/sets.py
+++ corepy/collections/sets.py
@@ -30,3 +30,29 @@ print(p)  # {3, 7, 9, 876, 176, 82}
 p.update([8, 9, 10])
 print(p)  # {3, 7, 8, 9, 10, 876, 176, 82}
 
+# set algebra - union, difference, intersection, subset, superset, disjoint
+blue_eyes = {'Olivia', 'Harry', 'Lily', 'Jack'}
+blond_hair = {'Harry', 'Jack', 'Amelia', 'Mia'}
+
+# union
+print(blond_hair.union(blue_eyes))  # {'Jack', 'Amelia', 'Mia', 'Olivia', 'Lily', 'Harry'}
+print(blond_hair.union(blue_eyes) == blue_eyes.union(blond_hair))  # True
+
+# difference
+print(blond_hair.difference(blue_eyes))  # {'Amelia', 'Mia'}
+print(blue_eyes.difference(blond_hair))  # {'Olivia', 'Lily'}
+
+# symmetric difference - people with blue eyes or blond hair, but not both
+print(blond_hair.symmetric_difference(blue_eyes))  # {'Amelia', 'Lily', 'Mia', 'Olivia'}
+
+# intersection
+print(blond_hair.intersection(blue_eyes))  # {'Jack', 'Harry'}
+
+# subset
+print({2, 3, 4}.issubset({1, 2, 3, 4, 5}))  # True
+
+# superset
+print({1, 2, 3, 4, 5}.issuperset({2, 3, 4}))  # True
+
+# disjoint
+print({1, 2, 3, 4, 5}.isdisjoint({6, 7, 8}))  # True
