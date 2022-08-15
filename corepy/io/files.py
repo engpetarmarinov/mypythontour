0a754b582c834bd653cc99398aed113266b1a764
Petar Marinov <mpetar@vmware.com>
2022-08-15 18:49:29 +0300

    files

---
+++ corepy/io/files.py
@@ -0,0 +1,27 @@
+import sys
+
+# writing into a file
+try:
+    f = open("wasteland.txt", mode="wt", encoding="utf-8")
+    f.write("What's going on?\n")
+    f.write("Am I writing into a file?\n")
+finally:
+    f.close()
+
+# reading from a file
+try:
+    f = open("wasteland.txt", mode="rt", encoding="utf-8")
+    for line in f:
+        sys.stdout.write(line)
+finally:
+    f.close()
+
+# appending to a file
+try:
+    f = open("wasteland.txt", mode="at", encoding="utf-8")
+    f.writelines([
+        "This line was appended!\n",
+        "This line was appended too!\n",
+    ])
+finally:
+    f.close()
