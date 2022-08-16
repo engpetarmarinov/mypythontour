114344e33b31a55a300f8d2f32b11bb5de297c73
Petar Marinov <mpetar@vmware.com>
2022-08-16 17:57:47 +0300

    dummy package

---
+++ organizing_larger_programs/importing_modules/dummypackage/__init__.py
@@ -0,0 +1,2 @@
+# required for python < 3.3
+print("dummypackage is being imported")
--- organizing_larger_programs/importing_modules/import_test.py
+++ organizing_larger_programs/importing_modules/import_test.py
@@ -6,7 +6,13 @@ from dummypackage import dummymodule
 print(dummypackage.__path__)
 # _NamespacePath(['/Users/mpetar/devwork/github.com/engpetarmarinov/mypythontour
 # /organizing_larger_programs/importing_modules/dummypackage'])
+print(dummypackage.__file__)
+# /Users/mpetar/devwork/github.com/engpetarmarinov/mypythontour/organizing_larger_programs/importing_modules/dummypackage/__init__.py
+print(dummymodule.__file__)
+# /Users/mpetar/devwork/github.com/engpetarmarinov/mypythontour/organizing_larger_programs/importing_modules/dummypackage/dummymodule.py
+
+print(type(dummypackage))  # <class 'module'>
+print(type(dummypackage.dummymodule))   # <class 'module'>
 
 print(dummymodule.__name__)  # dummypackage.dummymodule
 print(dummypackage.dummymodule.__name__)  # dummypackage.dummymodule
-
