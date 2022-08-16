d4a0e984ac1afbaab98363d77af4a47c73caed9a
Petar Marinov <mpetar@vmware.com>
2022-08-16 18:01:26 +0300

    Subpackage

--- organizing_larger_programs/importing_modules/import_test.py
+++ organizing_larger_programs/importing_modules/import_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # python3 import_test.py - will print dummy module
 import dummypackage
+import dummypackage.subpackage
 from dummypackage import dummymodule
 
 print(dummypackage.__path__)
@@ -10,7 +11,8 @@ print(dummypackage.__file__)
 # /Users/mpetar/devwork/github.com/engpetarmarinov/mypythontour/organizing_larger_programs/importing_modules/dummypackage/__init__.py
 print(dummymodule.__file__)
 # /Users/mpetar/devwork/github.com/engpetarmarinov/mypythontour/organizing_larger_programs/importing_modules/dummypackage/dummymodule.py
-
+print(dummypackage.subpackage.__file__)
+# /Users/mpetar/devwork/github.com/engpetarmarinov/mypythontour/organizing_larger_programs/importing_modules/dummypackage/subpackage/__init__.py
 print(type(dummypackage))  # <class 'module'>
 print(type(dummypackage.dummymodule))   # <class 'module'>
 
