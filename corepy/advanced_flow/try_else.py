7660f99ed1b1fa7bcc898746d16bf02e642dba8c
Petar Marinov <mpetar@vmware.com>
2022-09-24 14:55:11 +0300

    try else

---
+++ corepy/advanced_flow/try_else.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    try:
+        filename = "test.txt"
+        f = open(filename,"rt")
+    except OSError:
+        print(f"File {filename} could not be open for read.")
+    else:
+        # Now we are sure the file is open
+        num_lines = sum(1 for line in f)
+        print(f"Number of lines {num_lines}")
+        f.close()
+    finally:
+        print("We are done")
